feaeddffa95e6d56d57b9afef6e3e6cdc6bb6b1a
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2023-08-17 20:42:42 +0530

    rpa_account_viewer_frontend
    rpa_account_viewer_frontend/update on 17-08-2023

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace AccountDetailWindowsFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bdy_pnl = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dobLabel = new System.Windows.Forms.Label();
+            this.accountLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AccLabel = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ptName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.bdy_pnl.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1498, 72);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(154)))), ((int)(((byte)(139)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1437, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkGray;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.bdy_pnl);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1498, 1034);
+            this.panel2.TabIndex = 1;
+            // 
+            // bdy_pnl
+            // 
+            this.bdy_pnl.AllowDrop = true;
+            this.bdy_pnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bdy_pnl.BackColor = System.Drawing.Color.Beige;
+            this.bdy_pnl.Controls.Add(this.flowLayoutPanel1);
+            this.bdy_pnl.Controls.Add(this.panel5);
+            this.bdy_pnl.Controls.Add(this.panel4);
+            this.bdy_pnl.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.bdy_pnl.Location = new System.Drawing.Point(1034, 144);
+            this.bdy_pnl.Margin = new System.Windows.Forms.Padding(0);
+            this.bdy_pnl.Name = "bdy_pnl";
+            this.bdy_pnl.Size = new System.Drawing.Size(400, 400);
+            this.bdy_pnl.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 80);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 320);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.dobLabel);
+            this.panel5.Controls.Add(this.accountLabel);
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Controls.Add(this.AccLabel);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 39);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(400, 41);
+            this.panel5.TabIndex = 1;
+            // 
+            // dobLabel
+            // 
+            this.dobLabel.AutoSize = true;
+            this.dobLabel.Font = new System.Drawing.Font("Century", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dobLabel.Location = new System.Drawing.Point(287, 12);
+            this.dobLabel.Name = "dobLabel";
+            this.dobLabel.Size = new System.Drawing.Size(65, 23);
+            this.dobLabel.TabIndex = 4;
+            this.dobLabel.Text = "label5";
+            // 
+            // accountLabel
+            // 
+            this.accountLabel.Font = new System.Drawing.Font("Century", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accountLabel.Location = new System.Drawing.Point(134, 12);
+            this.accountLabel.Name = "accountLabel";
+            this.accountLabel.Size = new System.Drawing.Size(79, 20);
+            this.accountLabel.TabIndex = 3;
+            this.accountLabel.Text = "label2";
+            this.accountLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(233, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DOB:";
+            // 
+            // AccLabel
+            // 
+            this.AccLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AccLabel.Location = new System.Drawing.Point(23, 8);
+            this.AccLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.AccLabel.Name = "AccLabel";
+            this.AccLabel.Size = new System.Drawing.Size(122, 25);
+            this.AccLabel.TabIndex = 1;
+            this.AccLabel.Text = "Account No:";
+            this.AccLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Controls.Add(this.ptName);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(400, 39);
+            this.panel4.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(291, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ptName
+            // 
+            this.ptName.AutoSize = true;
+            this.ptName.Font = new System.Drawing.Font("Century", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ptName.Location = new System.Drawing.Point(85, 11);
+            this.ptName.Name = "ptName";
+            this.ptName.Size = new System.Drawing.Size(65, 23);
+            this.ptName.TabIndex = 1;
+            this.ptName.Text = "label4";
+            this.ptName.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1498, 1106);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Account Detail Form";
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.panel2.ResumeLayout(false);
+            this.bdy_pnl.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel bdy_pnl;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label AccLabel;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label dobLabel;
+        private System.Windows.Forms.Label accountLabel;
+        private System.Windows.Forms.Label ptName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Drawing.Drawing2D;
+
+namespace AccountDetailWindowsFormApp
+{
+    public partial class Form1 : Form
+    {
+        string AccountNumber;
+        string PatientFName;
+        string PatientLName;
+        string DOB;
+        public Form1()
+        {
+            InitializeComponent();
+            bdy_pnl.Visible = false;
+            ControlExtension.Draggable(button1, true);
+            ControlExtension.Draggable(bdy_pnl, true);
+            MakeButtonRound(button1);
+            
+
+            string filepath = @"C:\Users\DELL\source\repos\RPA_WinFormApp\readFile.json";
+            try
+            {
+                string jsonData = File.ReadAllText(filepath);
+                JArray jArray = JArray.Parse(jsonData);
+
+
+                foreach (JObject jObject in jArray)
+                {
+                    string Name = (string)jObject["Name"];
+                    string DueDate = (string)jObject["DueDate"];
+                    string Status = (string)jObject["Status"];
+                    AccountNumber = (string)jObject["Account Number"];
+                    PatientFName = (string)jObject["PtFname"];
+                    PatientLName = (string)jObject["PtLname"];
+                    DOB = (string)jObject["DOB"];
+
+                    accountLabel.Text = AccountNumber;
+                    ptName.Text = $"{PatientFName} {PatientLName}";
+                    dobLabel.Text = DOB;
+
+                    Panel dataPanel = new Panel();
+                    dataPanel.BorderStyle = BorderStyle.FixedSingle;
+                    dataPanel.MinimumSize = new Size(350, 80);
+                    dataPanel.Margin = new Padding(5);
+                    dataPanel.Padding = new Padding(5);
+                    dataPanel.AutoScroll = true;
+
+                    Label nameLabel = new Label();
+                    nameLabel.Text = $"Care Name: {Name}\n";
+                    nameLabel.Font =  new Font("Century Gothic", 10, FontStyle.Regular);
+                    nameLabel.AutoSize = true;
+
+                    Label dueDateLabel = new Label();
+                    dueDateLabel.Text = $"Due Date: {DueDate}\n";
+                    dueDateLabel.Font = new Font("Century Gothic", 10, FontStyle.Regular);
+                    dueDateLabel.AutoSize = true;
+
+                    Label statusLabel = new Label();
+                    statusLabel.Text = $"Status: {Status}";
+                    statusLabel.Font = new Font("Century Gothic", 10, FontStyle.Regular);
+                    statusLabel.AutoSize = true;
+
+                    nameLabel.Location = new Point(10, 10);
+                    dueDateLabel.Location = new Point(10, 30);
+                    statusLabel.Location = new Point(10, 50);
+
+                    dataPanel.Controls.Add(nameLabel);
+                    dataPanel.Controls.Add(dueDateLabel);
+                    dataPanel.Controls.Add(statusLabel);
+
+                    flowLayoutPanel1.Controls.Add(dataPanel);
+                    flowLayoutPanel1.AutoScroll = true;
+                    Console.WriteLine("Complete");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected Error :: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IsAccessible = true;
+            if (bdy_pnl.Visible)
+            {
+                bdy_pnl.Visible = false;
+            }
+            else { bdy_pnl.Visible = true;}
+           
+        }
+
+        private void MakeButtonRound(Button button)
+        {
+            int diameter = Math.Min(button.Width, button.Height);
+            button.Width = diameter;
+            button.Height = diameter;
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, diameter, diameter);
+
+            button.Region = new Region(path);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bdy_pnl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point newLock = new Point(e.X + bdy_pnl.Location.X, e.Y + bdy_pnl.Location.Y);
+                bdy_pnl.Location = newLock;
+            }
+        }
+
+     
+    }
+}
